db3c1f1146b67e0f3be8a40f16387808748d125e
zhengwenxu1995 <1501319100@qq.com>
2018-07-25 23:45:04 +0800

    商品列表修改1

--- commerce/src/views/Home/home.vue
+++ commerce/src/views/Home/home.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <headers></headers>
+    <nav-cont><span slot="navShow">Goods</span></nav-cont>
     <sort-price></sort-price>
     <goods-list></goods-list>
+    <foot></foot>
   </div>
 </template>
 
 <script>
+import  Headers from "@/components/header.vue"
+import  NavCont from "@/components/NavCont.vue"
+import  Foot from "@/components/foot.vue"
 import SortPrice from "@/views/Home/component/SortPrice.vue"
 import GoodsList from "@/views/Home/component/goodsList.vue"
 export default {
@@ -16,7 +22,10 @@ export default {
   },
   components:{
     SortPrice:SortPrice,
-    GoodsList:GoodsList
+    GoodsList:GoodsList,
+    Headers,
+    NavCont,
+    Foot
   }
 }
 </script>
