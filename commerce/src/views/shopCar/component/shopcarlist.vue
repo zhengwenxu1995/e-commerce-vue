fd0a82cf905cbd58884ca6dccb9cf3ff8b4e1bcc
zhengwenxu1995 <1501319100@qq.com>
2018-08-20 23:10:17 +0800

    购物车2

--- commerce/src/views/shopCar/component/shopcarlist.vue
+++ commerce/src/views/shopCar/component/shopcarlist.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        我是购物车列表
+        <div class="commodity-list">
+            <div class="commodity">
+                <ul>
+                    <li class="commodity-cont">
+                        <i class="iconfont">
+                            &#xe617;
+                        </i>
+                        <div class="">
+                            <img src="static/img/10.jpg" alt="" style="width:100px;height:100px;">
+                            <span>xiaoni</span>
+                        </div>
+                        </li>
+                    <li class="price"><span>2449</span></li>
+                    <li class="number">
+                        <a href="javascript:void(0);">-</a>
+                        <span>1</span>
+                        <a href="javascript:void(0);">+</a></li>
+                    <li class="sum">
+                        <span>2499</span>
+                    </li>
+                    <li class="del-btn">
+                        <a href="javascript:void(0)">
+                            <i class="iconfont">&#xe613;</i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="select-all">
+
+        </div>
     </div>
 </template>
 
