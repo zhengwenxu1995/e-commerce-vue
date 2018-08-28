b84b271fff73cf61897897446688b0f7ce8d4391
zhengwenxu <xu123456789@qq.com>
2018-08-28 17:59:26 +0800

    添加地址静态页面1

--- commerce/src/views/address/component/addressList.vue
+++ commerce/src/views/address/component/addressList.vue
@@ -2,18 +2,82 @@
     <div class="parent-frame">
         <div class="child-frame">
             <div class="step-cont">
-                <ul>
-                    <li class="Order"><span>aa</span></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <ul class="step-list">
+                    <li class="current-step"><h2 class="current-title">邮寄地址</h2></li>
+                    <li class="after-step"><h2 class="after-title">查看订单</h2></li>
+                    <li class="after-step"><h2 class="after-title">付款</h2></li>
+                    <li class="after-step"><h2 class="after-title">订单确认</h2></li>
                 </ul>
             </div>
-            <div class="address-list">
-aa 
+            <div class="address-cont">
+                <h1 class="address-title">地址列表</h1>
+                <div class="address-frame">
+                    <ul class="address-list">
+                        <li class="address-item">
+                            <div class="address-item-cont">
+                                <em class="address-user">姓名</em>
+                                <strong class="address-mail">朝阳公园</strong>
+                                <span class="tel">15932387701</span>
+                                <span class="default">默认</span>
+                                <i class="iconfont del"></i>
+                            </div>
+                        </li>
+                        <li class="address-item">
+                            <div class="address-item-cont">
+                                <em class="address-user">姓名</em>
+                                <strong class="address-mail">朝阳公园</strong>
+                                <span class="tel">15932387701</span>
+                                <span class="default">默认</span>
+                                <i class="iconfont del"></i>
+                            </div>
+                        </li>
+                        <li class="address-item">
+                            <div class="address-item-cont">
+                                <em class="address-user">姓名</em>
+                                <strong class="address-mail">朝阳公园</strong>
+                                <span class="tel">15932387701</span>
+                                <span class="default">默认</span>
+                                <i class="iconfont del"></i>
+                            </div>
+                        </li>
+                        <li class="address-item">
+                            <div class="address-item-cont">
+                                <em class="address-user">姓名</em>
+                                <strong class="address-mail">朝阳公园</strong>
+                                <span class="tel">15932387701</span>
+                                <span class="default">默认</span>
+                                <i class="iconfont del"></i>
+                            </div>
+                        </li>
+                        <li class="address-item">
+                            <div class="address-item-cont">
+                                <em class="address-user">姓名</em>
+                                <strong class="address-mail">朝阳公园</strong>
+                                <span class="tel">15932387701</span>
+                                <span class="default">默认</span>
+                                <i class="iconfont del"></i>
+                            </div>
+                        </li>
+
+                        <li class="add-address">
+                            <div class="add-address-cont">
+                                <div class="add-address"></div>
+                                <p class="add-">添加新地址</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div >
+                        <span>more<i class="iconfont more"></i></span>
+                    </div>
+                </div>
             </div>
             <div class="method-list">
-aa
+                <h1>邮寄方式</h1>
+                <div class="method-cont">
+                    <span>标准运输</span>
+                    <h2>自由</h2>
+                    <p>帆帆帆帆</p>
+                </div>
             </div>
         </div>
     </div>
@@ -46,16 +110,64 @@ export default {
 .parent-frame
     width :100%;
     background :#f5f7fc;
-    padding:1rem 0 2rem 0;
+    padding:0.8rem 0 2rem 0;
     .child-frame
         width :80%;
-        height :5rem;
         margin :0 auto;
+        .step-cont
+            padding : 1rem 0;
+            .step-list
+                display :flex;
+            .step-list li
+                flex :1;
+                height :0;
+                position :relative;
+                text-align :center;
+            .current-step:after 
+                content :"";
+                display:inline-block;
+                margin-top :-0.25rem;
+                height :0.5rem;
+                width :0.5rem;
+                background:#d1434a;
+                border-radius :100%;
+                z-index :2;
+            .after-step:after
+                content :"";
+                display:inline-block;
+                margin-top :-0.25rem;
+                height :0.5rem;
+                width :0.5rem;
+                background:#e0e0e0;
+                border-radius :100%;
+                z-index :2;
+            .step-list li h2
+                position :absolute;
+                font-size:0.4rem;
+                top:-0.8rem;
+                width :100%;
+                text-align :center;
+            .after-step
+                border:0.03rem solid #e0e0e0;
+            .current-step
+                 border:0.03rem solid #d1434a;
+            .after-step h2
+                color:#e0e0e0;
+            .current-step h2
+                color:#d1434a;
+        .address-cont
+            padding : 0.3rem 0 0 0;
+            .address-title
+                font-size :0.5rem;
+                padding-bottom :0.4rem;
+    .address-frame
+        width :100%;
         border :1px solid #f00;
-        .Order:before
-            content :"";
-            height :5px;
-            background :#dc4f47;
-            width :200px;
-
+        .address-list
+            width :100%;
+            display :flex;
+            flex-warp:warp;
+        .address-list li
+            width :25%;
+            border:1px solid #f005;
 </style>
